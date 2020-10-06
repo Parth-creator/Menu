8ac37db7ec0111ecb233a8e79cee7262eee1b7e1
Parth-creator <65723218+Parth-creator@users.noreply.github.com>
2020-10-06 21:59:17 +0530

    Add files via upload

--- menu_oop(2)/PapaZones.py
+++ menu_oop(2)/PapaZones.py
@@ -1,59 +1,79 @@
 class PapaZones:
     def __init__(self):
 
-        self.__burger_quantity = 0
-        self.__pizza_quantity = 0
-        self.__colddrink_quantity = 0
-        self.__menu_dict = {1: ['Burger', self.__burger_quantity, 10],
-                            2: ['Pizza', self.__pizza_quantity, 30],
-                            3: ['ColdDrink', self.__colddrink_quantity, 10]}
+        self.__order = {}
 
+        self.__menu_dict = {1: ['Burger', 10],
+                            2: ['Pizza', 30],
+                            3: ['ColdDrink', 10]}
 
     def ask_for_menu(self):
-        print("!!! Welcome to PapaZones !!!")
+
+        print("!!! Welcome to Zominoes !!!")
         yes_no = True
         while yes_no:
             print("Todays Menu:- ")
             for i in self.__menu_dict:
                 print(str(i) + ' ' + self.__menu_dict[i][0])
-            item_selector = input("Enter the item which you want by using serial number: \n")
 
-            if item_selector == '1':
-                print("You selected " + self.__menu_dict[1][0] + '.')
-                self.__menu_dict[1][1] = input("Enter Quantity: ")
-                print("You selected " + str(self.__burger_quantity) + ' ' + self.__menu_dict[1][0] + '.')
-                yes_no = input("Do you want to select more(Y/N): ")
+            item_selector = int(input("Enter the item which you want by using serial number: \n"))
 
-            if item_selector == '2':
-                print("You selected " + self.__menu_dict[2][0] + '.')
-                self.__menu_dict[2][1] = input("Enter Quantity: ")
-                print("You selected " + str(self.__burger_quantity) + ' ' + self.__menu_dict[2][0] + '.')
-                yes_no = input("Do you want to select more(Y/N): ")
+            quantity = 0
 
-            if item_selector == '3':
-                print("You selected " + self.__menu_dict[3][0] + '.')
-                self.__menu_dict[3][1] = input("Enter Quantity: ")
-                print("You selected " + str(self.__burger_quantity) + ' ' + self.__menu_dict[3][0] + '.')
-                yes_no = input("Do you want to select more(Y/N): ")
+            for key in self.__menu_dict:
+                if item_selector == key:
+                    print("You selected " + self.__menu_dict[key][0] + '.')
+
+                    quantity = quantity + int(input("Enter Quantity: "))
+
+                    print("You selected ", quantity, ' ', self.__menu_dict[key][0], '.')
+                    val2 = {self.__menu_dict[key][0]: quantity}
+                    self.__order.update(val2)
+
+            yes_no = input("Do you want to select more(Y/N): ")
 
             if yes_no == 'Y':
                 yes_no = True
 
             if yes_no == 'N':
-                self.total()
+                """print(1, '  ', self.__menu_dict[1][0], '  ', self.__order[self.__menu_dict[1][0]], '  ',
+                      self.__order[self.__menu_dict[1][0]] * self.__menu_dict[1][1])"""
+                self.__total()
+                # print(self.__order)
                 yes_no = False
 
     def __total(self):
-        print("Your total:- ")
-        print('1. ' + self.__menu_dict[1][0] + "      " + str(self.__menu_dict[1][1]) + "    " + str(
-            self.__menu_dict[1][2] * int(self.__menu_dict[1][1])))
-        print('2. ' + self.__menu_dict[2][0] + "       " + str(self.__menu_dict[2][1]) + "    " + str(
-            self.__menu_dict[2][2] * int(self.__menu_dict[2][1])))
-        print(
-            '3. ' + self.__menu_dict[3][0] + "   " + str(self.__menu_dict[3][1]) + "    " + str(
-                self.__menu_dict[3][2] * int(self.__menu_dict[3][1])))
-        print("TOTAL:-        " + str(
-            int(self.__menu_dict[1][1]) + int(self.__menu_dict[2][1]) + int(
-                self.__menu_dict[3][1])) + "    " + str(
-            self.__menu_dict[1][2] * int(self.__menu_dict[1][1]) + self.__menu_dict[2][2] * int(self.__menu_dict[2][1]) + self.__menu_dict[3][2] * int(
-                self.__menu_dict[3][1])))
+
+        keys = []
+        price = []
+        total_quantity = []
+        total_price = []
+        total_quantity_answer = 0
+        total_price_answer = 0
+
+        print("Your total: ")
+
+        for i in self.__order:
+            keys.append(i)
+
+        for i in range(len(keys)):
+            for j in self.__menu_dict:
+                if keys[i] == self.__menu_dict[j][0]:
+                    price.append(self.__menu_dict[j][1])
+
+        for i in range(len(self.__order)):
+            print(i + 1, '  ', keys[i], '  ', self.__order[keys[i]], '  ', self.__order[keys[i]] * price[i])
+
+        for i in range(len(self.__order)):
+            total_quantity.append(self.__order[keys[i]])
+
+        for i in total_quantity:
+            total_quantity_answer = total_quantity_answer + i
+
+        for i in range(len(self.__order)):
+            total_price.append(self.__order[keys[i]] * price[i])
+
+        for i in total_price:
+            total_price_answer = total_price_answer + i
+
+        print('TOTAL: ', total_quantity_answer, total_price_answer)
--- menu_oop(2)/welcome.py
+++ menu_oop(2)/welcome.py
@@ -2,9 +2,7 @@ from Zomino import *
 from Mcdonalds import *
 from PapaZones import *
 
-zomino = Zominoes()
-papa_zones = PapaZones()
-mcdonalds = Mcdonalds()
+res = None
 
 print("!!! Welcome to Zomino !!!")
 
@@ -12,13 +10,12 @@ print("Available restraurant: \n 1 Mcdonalds \n 2 Zomino \n 3 PapaZones")
 res_input = input("Enter the restraurant by using the serial number: \n")
 
 if res_input == '1':
-    mcdonalds.ask_for_menu()
+    res = Mcdonalds()
 
 if res_input == '2':
-    zomino.ask_for_menu()
+    res = Zominoes()
 
 if res_input == '3':
-    papa_zones.ask_for_menu()
+    res = PapaZones()
 
-
-
+res.ask_for_menu()
